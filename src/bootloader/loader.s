a0d26135146dd01ac79daecd77c5e65cd7afb030
maddie <144840450+llmaddie@users.noreply.github.com>
2025-10-08 20:59:53 +0200

    Update loader.s

--- src/bootloader/loader.s
+++ src/bootloader/loader.s
@@ -1,18 +1,18 @@
 global loader 
 
-    MAGIC_NUMBER equ 0x1BADB002
+    MAGIC_NUMBER equ 0x1BADB002         /* take the MAGIC_NUMBER of the bootloader */
     FLAGS        equ 0x0
     CHECKSUM     equ -MAGIC_NUMBER 
 
 
-section .text
+section .text       /* go to the .text section, recup MAGIC, FLAGS, and CHECKSUM  */
     align 4
     dd MAGIC_NUMBER
     dd FLAGS
     dd CHECKSUM
 
-loader: 
+loader:                 /* loader link to link.ld ( connected with the bootloader magic) */
 mov eax, 0xCAFEBABE
 
-.loop:
+.loop:              /* jump in loop on the section assigned  */
 jmp .loop
