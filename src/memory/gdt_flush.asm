69888603b49af73c86d3ddaa6148cbad87eb08e3
shyybi <charlotte.rod@outlook.com>
2025-10-12 22:32:08 +0200

    Refacto/fix: close () on comment

--- src/memory/gdt_flush.asm
+++ src/memory/gdt_flush.asm
@@ -4,7 +4,7 @@ gdt_flush:
     mov eax, [esp +4]           ; gdt_ptr adress
     lgdt [eax]                  ; load the GDT Pointer 
 
-    mov ax, 0x10                ; data segment selector (second entry so index=1 -> 0x05
+    mov ax, 0x10                ; data segment selector (second entry so index=1 -> 0x05)
     mov ds, ax
     mov es, ax
     mov fs, ax
