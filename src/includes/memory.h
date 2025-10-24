f68f81371499eaa4811ad0fce7e1f2421028ff30
llmaddie <contactmaddi3@gmail.com>
2025-10-24 21:36:50 +0200

    Patch memory.h to work with memory.c

--- src/includes/memory.h
+++ src/includes/memory.h
@@ -1,11 +1,23 @@
 #ifndef MEMORY_H
 #define MEMORY_H
 
-typedef unsigned int size_t;
-
-void *memset(void *ptr, int c, size_t size);
-int memcmp(void *s1, void *s2, int count);
-void *memcpy(void *dest, void *src, int len);
-void *memccpy(void *dest, void *src, int len, size_t n);
-
+// Définir size_t correctement pour votre architecture
+#if defined(__x86_64__) || defined(__aarch64__)
+    typedef unsigned long size_t;
+#else
+    typedef unsigned int size_t;
 #endif
+
+// Set memory zone to a specific value
+void *memset(void *ptr, int c, size_t size);
+
+// Compare two memory zones
+int memcmp(const void *s1, const void *s2, size_t count);
+
+// Copy memory zone
+void *memcpy(void *dest, const void *src, size_t len);
+
+// Copy up to n bytes or until character c is found
+void *memccpy(void *dest, const void *src, int c, size_t n);
+
+#endif // MEMORY_H
