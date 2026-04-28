51050ef598a663fd329c71c050355d45cdbf8b74
pepedinho <2spii94@gmail.com>
2026-04-28 11:49:46 +0200

    tweak: change memmove desc

--- src/lib/memory.c
+++ src/lib/memory.c
@@ -63,9 +63,7 @@ void *memcpy(void *dest, const void *src, size_t len) {
 }
 
 
-// The  memmove()  function copies n bytes from memory area src to memory area dest.  The memory areas may overlap: copying takes place
-// as though the bytes in src are first copied into a temporary array that does not overlap src or dest, and the bytes are then  copied
-// from the temporary array to dest.
+// Cpoy memory zone taking into account the overlap
 void *memmove(void *dest, const void *src, size_t n) {
     unsigned char *d = (unsigned char *)dest;
     const unsigned char *s = (const unsigned char *)src;
