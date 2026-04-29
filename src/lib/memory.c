521ce99d51057435c39705bccdec648d37c39be2
Xql.dev <205849173+KirobotDev@users.noreply.github.com>
2026-04-30 06:05:31 +0700

    Update memory.c

--- src/lib/memory.c
+++ src/lib/memory.c
@@ -100,6 +100,8 @@ void *memccpy(void *dest, const void *src, int c, size_t n) {
 // Duplicate a string pointer
 char *strdup(const char *src)
 {
+	if (src == NULL)
+		return NULL;
 	char *cpy = malloc(strlen(src) + 1);
 	if (cpy == NULL)
 		return NULL;
