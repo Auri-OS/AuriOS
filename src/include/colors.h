c75f3fe951380eb16864c4baa06db2ee633ceeef
shyybi <charlotte.rod@outlook.com>
2026-05-06 02:08:50 +0200

    feat: create color library, setting the user state to a char variable cli_nav[]

---
+++ src/include/colors.h
@@ -0,0 +1,43 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+// Font Colors
+#define COLOR_BLACK "\x1b[30m"
+#define COLOR_RED "\x1b[31m"
+#define COLOR_GREEN "\x1b[32m"
+#define COLOR_YELLOW "\x1b[33m"
+#define COLOR_BLUE "\x1b[34m"
+#define COLOR_MAGENTA "\x1b[35m"
+#define COLOR_CYAN "\x1b[36m"
+#define COLOR_WHITE "\x1b[37m"
+#define COLOR_GREY "\x1b[90m"
+#define COLOR_RED_BRIGHT "\x1b[91m"
+#define COLOR_GREEN_BRIGHT "\x1b[92m"
+#define COLOR_YELLOW_BRIGHT "\x1b[93m"
+#define COLOR_BLUE_BRIGHT "\x1b[94m"
+#define COLOR_MAGENTA_BRIGHT "\x1b[95m"
+#define COLOR_CYAN_BRIGHT "\x1b[96m"
+#define COLOR_WHITE_BRIGHT "\x1b[97m"
+
+// Background Colors
+#define BG_COLOR_BLACK "\x1b[40m"
+#define BG_COLOR_RED "\x1b[41m"
+#define BG_COLOR_GREEN "\x1b[42m"
+#define BG_COLOR_YELLOW "\x1b[43m"
+#define BG_COLOR_BLUE "\x1b[44m"
+#define BG_COLOR_MAGENTA "\x1b[45m"
+#define BG_COLOR_CYAN "\x1b[46m"
+#define BG_COLOR_WHITE "\x1b[47m"
+#define BG_COLOR_GREY "\x1b[100m"
+#define BG_COLOR_RED_BRIGHT "\x1b[101m"
+#define BG_COLOR_GREEN_BRIGHT "\x1b[102m"
+#define BG_COLOR_YELLOW_BRIGHT "\x1b[103m"
+#define BG_COLOR_BLUE_BRIGHT "\x1b[104m"
+#define BG_COLOR_MAGENTA_BRIGHT "\x1b[105m"
+#define BG_COLOR_CYAN_BRIGHT "\x1b[106m"
+#define BG_COLOR_WHITE_BRIGHT "\x1b[107m"
+
+// Basic white color
+#define COLOR_RESET "\x1b[0m"
+
+#endif
