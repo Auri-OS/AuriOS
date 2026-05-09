11c8f56539a088aec0a6bf97447cae591a67cc3c
Max_auCube <limason@gmx.fr>
2026-05-09 11:23:00 +0200

    feat: page fault handling

--- src/cpu/isr.c
+++ src/cpu/isr.c
@@ -1,6 +1,8 @@
 #include "../include/isr.h"
 #include "../include/terminal.h"
 #include "../include/log.h"
+#include <stdbool.h>
+
 
 static const char *exception_messages[32] = {
     "Division By Zero",
@@ -37,12 +39,37 @@ static const char *exception_messages[32] = {
     "Reserved"
 };
 
+// Helper function to read CR2 register
+static inline uintptr_t read_cr2(void)
+{
+    uintptr_t cr2;
+    __asm__ __volatile__("mov %%cr2, %0" : "=r" (cr2));
+    return cr2;
+}
+
 void isr_handler(registers_t *regs)
 {
+    if (regs->int_no == 14){
+        uintptr_t faulting_address = read_cr2();
+        
+        bool present_violation = (regs->err_code & 0x1);
+        bool write_violation   = (regs->err_code & 0x2);
+        bool user_mode         = (regs->err_code & 0x4);
+
+        terminal_writestring("[PANIC] Page fault at ");
+        terminal_writehex(faulting_address);
+        terminal_writestring("\nReason:");
+        terminal_writestring(present_violation ? "Protection-Violation " : "Page-Not-Present ");
+        terminal_writestring(write_violation ? "Write-Access " : "Read-Access ");
+        terminal_writestring(user_mode ? "User-Mode" : "Supervisor-Mode");
+        
+        terminal_writestring("\n");
+    }
     if (regs->int_no < 32) {
         terminal_writestring("EXCEPTION: ");
         terminal_writestring(exception_messages[regs->int_no]);
         terminal_writestring("\n");
+        
         KPANIC(exception_messages[regs->int_no]);
         for (;;) {
           asm volatile("cli; hlt");
