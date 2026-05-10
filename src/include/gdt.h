399f4ada234b90592fdf5d80947f139962b415ea
pepedinho <2spii94@gmail.com>
2026-05-10 15:19:45 +0200

    gdt: add tss for context switching

--- src/include/gdt.h
+++ src/include/gdt.h
@@ -37,6 +37,24 @@ struct __attribute__((packed)) gdt_ptr {
     uint32_t base;
 };
 
+
+struct tss_entry {
+    uint32_t prev_tss;
+    uint32_t esp0;
+    uint32_t ss0;
+    uint32_t esp1;
+    uint32_t ss1;
+    uint32_t esp2;
+    uint32_t ss2;
+    uint32_t cr3;
+    uint32_t eip, eflags, eax, ecx, edx, ebx, esp, ebp, esi, edi;
+    uint32_t es, cs, ss, ds, fs, gs;
+    uint32_t ldt;
+    uint16_t trap;
+    uint16_t iomap_base;
+} __attribute__((packed));
+
+
 uint64_t create_descriptor(uint32_t base, uint32_t limit, uint16_t flag);
 void gdt_init(void);
 
