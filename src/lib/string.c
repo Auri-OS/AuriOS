2386d46d0422ceb090ddacaa3e604fc8ae2cee25
shyybi <charlotte.rod@outlook.com>
2026-05-18 14:56:43 +0200

    fix: correcting size length counting

--- src/lib/string.c
+++ src/lib/string.c
@@ -122,7 +122,7 @@ char *strlcpy(char *dest, const char *src, size_t size) {
     size_t i = 0;
     if (size == 0)
         return dest;
-    while (i < size - 1 && src[i] != '\0') {
+    while (i < size && src[i] != '\0') {
         dest[i] = src[i];
         i++;
     }
