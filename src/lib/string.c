cb3da4236eb70eead596f94f79a0e769df4019eb
Charlotte <Charlotte>
2026-08-29 16:53:40 +0200

    feat: implement tab completion for shell commands refactor: update strncmp function signature for consistency

--- src/lib/string.c
+++ src/lib/string.c
@@ -19,7 +19,7 @@ int strcmp(const char *s1, const char *s2)
 }
 
 // Compare the first n byte of two string between them
-int	strncmp(char *s1, char *s2, unsigned int n)
+int	strncmp(const char *s1, char *s2, unsigned int n)
 {
 	unsigned int i = 0;
 	if (n == 0)
